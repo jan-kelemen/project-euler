145efaa2b1d986979fb6194904c387ba5ec691e5
Jan Kelemen <jkeleme@gmail.com>
2015-02-14 20:53:04 +0100

    Problem 10 optimization

--- problem_solutions/010_py_summation_of_primes/_010_py_summation_of_primes.py
+++ problem_solutions/010_py_summation_of_primes/_010_py_summation_of_primes.py
@@ -1,23 +1,15 @@
-import math
-
-numbers = [False, False, True] + 999999*[True, False]
-primes = [2]
-
 def sieve():
-    limit = round(math.sqrt(len(numbers) - 1)) + 1
-    for x in range(3, len(numbers), 2):
-        if numbers[x] == True:
-            primes.append(x)
-            if x < limit:
-                for y in range(x + 2, len(numbers), 2):
-                    if numbers[y] == True and y % x == 0:
-                        numbers[y] = False
+    numbers = [False, False, True] + 999999*[True, False]
+    for odd in range(3, len(numbers)//2 + 1, 2):
+        if numbers[odd] == False:
+            continue
+        for multiple in range(2*odd, len(numbers), odd):
+            numbers[multiple] = False
+
+    return [x for x in range(len(numbers)) if numbers[x] == True]
 
 def sum_primes():
     sieve()
-    sum = 0
-    for prime in primes:
-        sum += prime
-    return sum
+    return sum(sieve())
 
 print(sum_primes())
